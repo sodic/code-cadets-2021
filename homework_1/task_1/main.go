9312095b7805cef6af4410474c1728e6f9a444c3
Filip Sodić <filip.sodic@fer.hr>
2021-05-26 13:19:32 +0200

    Solve task 1

---
+++ homework_1/task_1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"code-cadets-2021/homework_1/task_1/fizzbuzz"
+)
+
+var rules = []fizzbuzz.Rule{
+	{Divisor: 3, Value: "Fizz"},
+	{Divisor: 5, Value: "Buzz"},
+}
+
+func main() {
+	start, end := parseArgs()
+
+	result, err := fizzbuzz.GetSequence(start, end, rules)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(strings.Join(result, ", "))
+}
+
+func parseArgs() (int, int) {
+	start := flag.Int("start", 1, "The start of the FizzBuzz range.")
+	end := flag.Int("end", 20, "The end of the FizzBuzz range.")
+	flag.Parse()
+	return *start, *end
+}
